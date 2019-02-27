15fcb1a1a7eb3bc4e2c567924acaf708668bdfd9
aledic85 <dicuollo85@gmail.com>
2019-02-27 17:44:57 +0100

    some additions

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ var button = $('.btn-rnd');
 var div = $('.num-cont');
 var clicks = 0;
 
-button.click(function() {
+button.on({mouseenter: function() {
+  div.css({"height": "700px", "width": "800px"})
+}, mouseleave: function(){
+  div.css({"height": "auto", "width": "300px"})
+}, click: function() {
   clicks +=1;
   var rnd = Math.floor(Math.random() * 100) + 1;
   div.html(div.html() + " " + rnd);
@@ -12,4 +16,4 @@ button.click(function() {
     div.css({"color": "black", "background": "red" });
   }
   button.html("Genera numero" + " " + clicks);
-})
+}})
